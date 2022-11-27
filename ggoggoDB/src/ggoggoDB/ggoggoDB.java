fb6530c7b0fe5e088e31e15d94baab82d70a09c0
hyeriimm <chr121910@gmail.com>
2022-11-27 14:32:42 +0900

    phase3

--- ggoggoDB/src/ggoggoDB/ggoggoDB.java
+++ ggoggoDB/src/ggoggoDB/ggoggoDB.java
@@ -3,96 +3,188 @@ package ggoggoDB;
 import java.sql.*;
 import java.util.Scanner;
 
-public class ggoggoDB {
-	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	public static final String USER_UNIVERSITY ="ggoggoDB";
-	public static final String USER_PASSWD ="comp322";
-	
-	// public static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
-	// public static final String USER_DBPROJ = "dbproject";
-	// public static final String USER_PASSWD = "db";
+public class InterestedGenresSetting {
 
-	public static void main(String[] args) {
-		Connection conn = null; // Connection object
-		Scanner scan = new Scanner(System.in);
-		
+    private static void show_interested_genre (Connection conn, UserInfo info, boolean reverse) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int cnt = 0;
+		int oneline_max = 6;
+		String sql = "";
+		String print_memo = "";
+
+		if(reverse){
+			sql = "(select g.genrename \n"
+				+ "from Genre G, interested I\n"
+				+ "where G.Genreid = I.Genreid)\n"
+				+ "minus\n"
+				+ "(select g.genrename \n"
+				+ "from Genre G, interested I\n"
+				+ "where pjuserid = ?\n"
+				+ "and G.Genreid = I.Genreid)";
+			print_memo = "<< Your Not Interested Genre >>";
+		}else{
+			sql = "select g.genrename \n"
+				+ "from Genre G, interested I\n"
+				+ "where pjuserid = ?\n"
+				+ "and G.Genreid = I.Genreid";
+			print_memo = "<< Your Interested Genre >>";
+		}
 		try {
-			// Load a JDBC driver for Oracle DBMS
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			// Get a Connection object 
-			//System.out.println("Success!");
-		}catch(ClassNotFoundException e) {
-			System.err.println("error = " + e.getMessage());
-			System.exit(1);
-		}
-
-		// Make a connection
-		try{
-			conn = DriverManager.getConnection(URL, USER_UNIVERSITY, USER_PASSWD); 
-			//System.out.println("Connected.");
-			conn.setAutoCommit(false);
-		}catch(SQLException ex) {
-			ex.printStackTrace();
-			System.err.println("Cannot get a connection: " + ex.getLocalizedMessage());
-			System.err.println("Cannot get a connection: " + ex.getMessage());
-			System.exit(1);
-		}
-		
-	
-		/* 1. 로그인*/
- 		UserInfo login = Login.login(conn, scan);
-		//System.out.println(login.isVaild());
-		//System.out.println(login.getID());
-		
-		/* 2. 장르 설정*/
-		if (login.isVaild()) {
-			System.out.println("로그인 완료");
-			System.out.println("장르설정");
-			InterestedGenresSetting.edit_interested_genre(conn, login, scan);
-
-			OpenBoard openboard = new OpenBoard(conn);
-			MyPage mypage = new MyPage(conn, login.getID());
-
-			while(true){
-				System.out.println("0.선호 장르 변경 1. 검색, 2. 타임라인 확인, 3. 게시판 확인, 4.마이페이지 5.종료");
-				int select = scan.nextInt();
-				scan.nextLine();
-				switch(select){
-					case 0:
-						InterestedGenresSetting.edit_interested_genre(conn, login, scan);
-						break;
-					case 1:
-						FilterInfo temp = Searching.settingFilter(conn, scan);
-						Searching.search(conn, scan, temp);
-						break;
-					case 2:
-						TimeLine.show(conn,login.getID());
-						break;
-					case 3:
-						openboard.executeOpenBoard(conn, scan, login.getID());
-						break;
-					case 4:
-						mypage.executeMyPage(conn, scan);
-
-						break;
-					case 5:
-						break;
-				}
-				if(select==5){
-					break;
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, info.getID());
+			rs = pstmt.executeQuery();
+			
+			System.out.println(print_memo);
+			while(rs.next()) {
+				String Genre = rs.getString(1);
+				System.out.printf("%25s  ",Genre);
+				cnt++;
+				if(cnt == oneline_max){
+					cnt = 0;
+					System.out.println();
 				}
 			}
 			
-		}
-		System.out.println("프로그램 종료");
-
-		try {
-			scan.close();
-			conn.close();
+			System.out.println();
+			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private static int genre_name_to_id (Connection conn, String gname) {
+		int id = -1;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "select genreid\n"
+					+ "from genre\n"
+					+ "where genrename = ?";
+		try{
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, gname);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				id = rs.getInt(1);
+			}
+			rs.close();
+			pstmt.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+
+		return id;
+	}
+	
+	private static boolean is_there_tuple (Connection conn, String userID, int gID) {
+		boolean result = false;
+
+		PreparedStatement pstmt = null; 
+		ResultSet rs = null;
+		
+		String sql = "select * \n"
+				+ "from interested \n"
+				+ "where pjUserid = ?\n"
+				+ "and Genreid = ?";
+		try{
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			pstmt.setInt(2, gID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = true;
+			}
+			pstmt.close();
+			rs.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	private static void insert_interested_table (Connection conn, String userID, int gID) {
+		try{
+			String sql = "insert into INTERESTED values (?, ?)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			pstmt.setInt(2, gID);
+			int res = pstmt.executeUpdate();
+			if(res == 1) {
+				conn.commit();
+			}
+			else
+				System.out.println("error!");
+			pstmt.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	private static void delete_interested_table (Connection conn, String userID, int gID) {
+		PreparedStatement pstmt = null; 
+		String sql = "delete from INTERESTED where PJUSERID=? and GENREID=?";
+		try{
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			pstmt.setInt(2, gID);
+			pstmt.executeUpdate();
+			pstmt.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void edit_interested_genre (Connection conn, UserInfo info, Scanner scan ) {
+		String genre = "start";
+		int gID = -1;
+		
+		while (true) {
+			System.out.println("********* 2 선호장르 변경 *********");
+			
+			// 1. 선호 장르 보여주기
+			System.out.println("다음은 당신의 선호 장르입니다.");
+			show_interested_genre(conn, info ,false);
+
+			// 2. 비선호 장르 보여주기
+			System.out.println("다음은 당신의 비선호 장르입니다.");
+			show_interested_genre(conn, info, true);
+			
+			// 3. 선호도를 바꾸기를 원하는 장르를 string 형태로 받아들이도록 함
+			System.out.println("위의 리스트의 장르 중 선호유무를 변경시킬 장르를 입력하세요.");
+			System.out.println("(설정 변경의 종료를 원한다면 'end'를 입력하세요.)");
+			genre = scan.nextLine();
+			if(genre.equals("end")){
+				break;
+			}
+
+			// 4. 바꾸기로 한 장르의 id를 찾고 interested에 유저가 id 장르를 선호하는지 확인한다.
+			gID = genre_name_to_id(conn, genre);
+			while(gID == -1){
+				System.out.println("잘못 입력하셨습니다 다시 입력해주세요.");
+				genre = scan.nextLine();
+				if(genre.equals("end")){
+					break;
+				}
+				gID = genre_name_to_id(conn, genre);
+			}
+			
+			if(genre.equals("end")){
+				break;
+			}
+
+			// 5. 해당 tuple이 있다면 table에서 삭제하고, 없다면 삽입한다. 
+			String userID = info.getID();
+			if( is_there_tuple(conn, userID, gID) ){
+				delete_interested_table(conn, userID, gID);
+			}else{
+				insert_interested_table(conn, userID, gID);
+			}
+		}
+
+		System.out.println("********* 장르설정 종료 *********");
+	}
+
 }
-
-
